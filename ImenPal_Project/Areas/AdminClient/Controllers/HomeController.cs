9017918e768a5ace1469bff2d92780515244dd99
karimnemati <karim.nemati17@gmail.com>
2021-01-11 10:44:58 +0330

    Commit

--- ImenPal_Project/Areas/AdminClient/Controllers/HomeController.cs
+++ ImenPal_Project/Areas/AdminClient/Controllers/HomeController.cs
@@ -13,12 +13,7 @@ namespace ImenPal_Project.AdminClient.Controllers
 
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+      
 
         public IActionResult Index()
         {
@@ -30,10 +25,5 @@ namespace ImenPal_Project.AdminClient.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ ImenPal_Project/Areas/AdminClient/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ImenPal_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ImenPal_Project.AdminClient.Controllers {
+    [Area ("AdminClient")]
+
+    public class LoginController : Controller {
+
+        public IActionResult Login () {
+            return View ();
+        }
+  public IActionResult LoginCode () {
+            return View ();
+        }
+        public IActionResult signup () {
+            return View ();
+        }
+        public IActionResult forgot_pwd () {
+            return View ();
+        }
+        public IActionResult signupCode () {
+            return View ();
+        }
+
+           public IActionResult signupEnd () {
+            return View ();
+        }
+
+    }
+}
